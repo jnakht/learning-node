adc13834ffaaf383f01788753c544d9f9c05c12e
jnakht <jisan.cse.cu@gmail.com>
2025-05-31 22:25:34 +0600

    module 12.9

---
+++ File-1.js
@@ -0,0 +1,11 @@
+const test_var = require('./File-2');
+
+const {a, add, b} = test_var;
+
+console.log(test_var.a);
+console.log(test_var.add(3, 5));
+console.log(b);
+console.log(a);
+console.log(add(7, 7));
+console.log(test_var);
+
