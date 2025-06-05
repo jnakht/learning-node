d60249a7e99ebe206081cf25492b298f0f72c4ed
jnakht <jisan.cse.cu@gmail.com>
2025-06-05 21:17:01 +0600

    module 12.9 recap

---
+++ module12RECAP/file-1.js
@@ -0,0 +1,21 @@
+// const var1 = require('./file-2');
+
+const {a, b,  add} = require('./file-2')
+// console.log(var1);
+// console.log(var1.a);
+// console.log(var1);
+// console.log(var1.add(2, 3));
+
+console.log(a);
+console.log(add);
+console.log(add(4, 3));
+console.log(b);
+
+
+
+const x = 109;
+
+module.exports = {x};
+console.log(module);
+
+
---
+++ module12RECAP/file-2.js
@@ -0,0 +1,12 @@
+
+
+const a = 10;
+const add = (param1, param2) => param1 + param2;
+const b = 444;
+// module.exports = a;
+module.exports = {
+    a,
+    add,
+    b
+}
+console.log(module);
