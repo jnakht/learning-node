b29ea693885011622efb8199dbe3a8afa7627e76
jnakht <jisan.cse.cu@gmail.com>
2025-06-11 00:50:39 +0600

    module 13.7

--- module13RECAP/ToDo-app/server.js
+++ module13RECAP/ToDo-app/server.js
@@ -2,8 +2,17 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req, res);
-    res.end('Welcome to ToDo App');
+    // console.log(req, res);
+    // console.log(req.url, req.method);
+    // res.end('Welcome to ToDo App');
+
+    if (req.url === '/todos' && req.method === 'GET') {
+        res.end('All ToDos');
+    } else if (req.url === '/todos/create-todo' && req.method === 'POST') {
+        res.end('Create ToDo');
+    } else {
+        res.end('Route Not Found');
+    }
 })
 
 server.listen(5000, '127.0.0.1', () => {
