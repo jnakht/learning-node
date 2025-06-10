fafdfb6fafa3f316a005960a149a430a18d9af3f
jnakht <jisan.cse.cu@gmail.com>
2025-06-11 00:37:11 +0600

    module 13.6 done

---
+++ module13RECAP/ToDo-app/server.js
@@ -0,0 +1,11 @@
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log(req, res);
+    res.end('Welcome to ToDo App');
+})
+
+server.listen(5000, '127.0.0.1', () => {
+    console.log('Server is listening on port: 5000');
+})
