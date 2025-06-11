69991e4271dacff69939e7f32a5f8dc3ebb7be94
jnakht <jisan.cse.cu@gmail.com>
2025-06-11 13:03:08 +0600

    module 13.8 recap

--- module13RECAP/ToDo-app/server.js
+++ module13RECAP/ToDo-app/server.js
@@ -1,13 +1,46 @@
 
 const http = require('http');
 
+
+const data = [{
+            "todos": "first Todo",
+            "id": "1",
+        }];
 const server = http.createServer((req, res) => {
     // console.log(req, res);
     // console.log(req.url, req.method);
     // res.end('Welcome to ToDo App');
 
     if (req.url === '/todos' && req.method === 'GET') {
-        res.end('All ToDos');
+        // res.writeHead(200, {
+        //     "content-type": 'text/plain'
+        // })
+        // res.end('jdfj');
+
+        // res.writeHead(201, {
+        //     "content-type": 'application/json',
+        //     "email": 'jisan@gmail.com',
+        // })
+        // res.setHeader("content-type", 'application/json');
+        // res.setHeader("email", 'nadim@gmail.com');
+        // res.statusCode = 201;
+        // // res.end(JSON.stringify({
+        // //     "todos": "first Todo",
+        // //     "id": "1",
+        // // }))
+        // res.end(JSON.stringify(data));
+
+        res.writeHead(200, {
+            "content-type": 'text/html',
+        })
+        res.end(`
+            <h1> Welcomet to ToDo App</h1>
+            <p> Here is your todo list</p>
+            <ul> 
+                <li>First Todo</li>
+                <li>Second Todo</li>
+            </ul>
+        `)
     } else if (req.url === '/todos/create-todo' && req.method === 'POST') {
         res.end('Create ToDo');
     } else {
